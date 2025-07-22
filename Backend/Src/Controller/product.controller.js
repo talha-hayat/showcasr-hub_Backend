cacd919eeb68fbd905d7b1fa261965b97ded2ee3
talha-hayat <taqimadakhail9999@gmail.com>
2025-07-22 11:50:40 -0700

    implement edit funtionality

--- Backend/Src/Controller/product.controller.js
+++ Backend/Src/Controller/product.controller.js
@@ -37,8 +37,28 @@ export const getProductsbyid = async (req, res) => {
 export const deleteProductsbyid = async (req, res) => {
   try {
     const id = req.params.id; // Get the product ID from the request parameters
-    const products = await Product.findByIdAndDelete(id); // Mongoose se data fetch
+    const products = await Product.findByIdAndDelete(id); 
     res.status(200).json({message: "Product deleted successfully"});
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+export const editProductsbyid = async (req, res) => {
+  try {
+    const id = req.params.id; // Get the product ID from the request parameters
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      req.body, // Update with data from the request body
+      { new: true, runValidators: true } // Options: return the updated document and run schema validators
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({ message: "Product edited successfully", product: updatedProduct });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- Backend/Src/Routes/product.route.js
+++ Backend/Src/Routes/product.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import  { createProduct , deleteProductsbyid, getAllProducts, getProductsbyid } from "../Controller/product.controller.js"
+import  { createProduct , deleteProductsbyid, editProductsbyid, getAllProducts, getProductsbyid } from "../Controller/product.controller.js"
 
 const productRoute = express.Router();
 
@@ -7,5 +7,6 @@ productRoute.post("/",createProduct)
 productRoute.get('/', getAllProducts);
 productRoute.get('/:id', getProductsbyid);
 productRoute.delete('/:id', deleteProductsbyid);
+productRoute.put('/:id', editProductsbyid);
 
 export default productRoute;
